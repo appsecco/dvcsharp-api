7c5743d66afab517b0f963f79128360b1aac2c27
ad <ad@localhost>
2018-05-16 17:37:39 +0530

    Added authentication bypass bug in Authorization Controller

--- Controllers/AuthorizationsController.cs
+++ Controllers/AuthorizationsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 using dvcsharp_core_api.Models;
 using dvcsharp_core_api.Data;
 
@@ -32,6 +33,38 @@ namespace dvcsharp_core_api
          if(response == null) {
             return Unauthorized();
          }
+
+         return Ok(response);
+      }
+
+      [HttpGet("GetTokenSSO")]
+      public IActionResult GetTokenSSO()
+      {
+         var ssoCookieData = HttpContext.Request.Cookies["sso_ctx"];
+
+         if(String.IsNullOrEmpty(ssoCookieData)) {
+            return Unauthorized();
+         }
+
+         var ssoCookieDecoded = Convert.FromBase64String(ssoCookieData);
+         var ssoCookie = JObject.Parse(System.Text.Encoding.UTF8.GetString(ssoCookieDecoded));
+
+         var userId = ssoCookie["auth_user"];
+         if(userId == null) {
+            return Unauthorized();
+         }
+
+         var user = _context.Users.
+            Where(b => b.ID == userId.ToObject<int>()).
+            FirstOrDefault();
+
+         if(user == null) {
+            return NotFound();
+         }
+
+         var response = new Models.AuthorizationResponse();
+         response.role = user.role;
+         response.accessToken = user.createAccessToken();
 
          return Ok(response);
       }

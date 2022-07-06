58aa5c5917e3db9d9005db0414c0b17d8e3ff9ef
Sam Fahnestock <sfahnestock@softwaredesignsolutions.com>
2022-07-06 13:30:43 -0400

    Fix bad code

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -68,15 +68,12 @@ namespace dvcsharp_core_api
             return Ok("Cannot search without a keyword");
          }
 
-         var query = $"SELECT * From Products WHERE name LIKE '%{keyword}%' OR description LIKE '%{keyword}%'";
-         var products = _context.Products
-            .FromSql(query)
-            .ToList();
+            //var query = $"SELECT * From Products WHERE name LIKE '%{keyword}%' OR description LIKE '%{keyword}%'";
+            //var products = _context.Products
+            //   .FromSql(query)
+            //   .ToList();
 
-        var query2 = $"SELECT * From Products WHERE name LIKE '%{keyword}%' OR description LIKE '%{keyword}%'";
-        var products2 = _context.Products
-            .FromSql(query)
-            .ToList();
+            var products = _context.Products.ToList();
 
         return Ok(products);
       }
